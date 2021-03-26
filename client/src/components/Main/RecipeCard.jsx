658b7f44c32b9322cd992e8f326f79a88c45cf5c
Oleg-Sachuk <Sachuk.o.a@gmail.com>
2021-03-26 16:58:31 +0200

    Updated main page layout

--- client/src/components/Main/RecipeCard.jsx
+++ client/src/components/Main/RecipeCard.jsx
@@ -1,44 +1,26 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Card, CardDeck } from 'react-bootstrap';
-import { useUpload } from '../../hooks/upload.hook';
+import React from 'react';
+import { Card, Dropdown, DropdownButton } from 'react-bootstrap';
 import style from './MainPage.module.css';
 
 const ReciptCard = (props) => {
-
-    const ImageData = useRef(null);
-    const {request} = useUpload();
-    let Image = useRef(null);
-    const [loaded, setLoaded] = useState(false)
-
-    const getImage = useCallback( async () =>{
-        try {
-            if(loaded === false) {
-                const image = await request(`/api/files/image/${props.instance.pictures[0]}`, 'GET', null);
-                Image.current = image;
-                debugger;
-                setLoaded(true);
-            }
-        } catch (error) {
-            
-        }
-    }, [request,props.instance.pictures,loaded])
-    
-    useEffect( () => {
-        getImage()
-    },[getImage])
+    const imgName = props.instance.pictures[0];
+    const date = `${imgName.substring(0, 2)}.${imgName.substring(2, 4)}.${imgName.substring(4, 8)}`
 
     return (
         <div>
             <Card className={style.cardBody}>
-                <Card.Img variant="top" src={`/api/files/image/${props.instance.pictures[0]}`} />
+                <DropdownButton className={style.dropdownBtn} id="dropdown-item-button" >
+                    <Dropdown.ItemText>{props.instance.description}</Dropdown.ItemText>
+                </DropdownButton>
+                <Card.Img variant="top" src={`/api/files/image/${imgName}`} />
                 <Card.Body>
                     <Card.Title>{props.instance.name}</Card.Title>
                     <Card.Text>
-                        <p>{props.instance.description}</p>
+                        
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted">Posted: {date}</small>
                 </Card.Footer>
             </Card>
         </div>
